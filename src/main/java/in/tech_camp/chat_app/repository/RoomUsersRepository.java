b8f15cc7c64780d87599a30e00340d97dda288d9
misa-beabl <misaki.inoue@smart-group.co.jp>
2025-05-13 09:29:59 +0900

    チャットルーム作成機能の実装

--- src/main/java/in/tech_camp/chat_app/repository/RoomUsersRepository.java
+++ src/main/java/in/tech_camp/chat_app/repository/RoomUsersRepository.java
@@ -1,8 +1,14 @@
 package in.tech_camp.chat_app.repository;
 
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
+
+import in.tech_camp.chat_app.entity.RoomUsersEntity;
 
 @Mapper
 public interface RoomUsersRepository {
-  
+  @Insert("INSERT INTO room_users(user_id, room_id) VALUES(#{user.id}, #{room.id})")
+  @Options(useGeneratedKeys = true, keyProperty = "id")
+  void insert(RoomUsersEntity userRoomEntity);
 }
